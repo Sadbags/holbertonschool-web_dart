456daf439317caba7a157310e233b6a15b9d5b2c
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-12-13 20:35:47 -0400

    add files

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,4 +1,8 @@
+String inner(String name, String id) {
+  var parts = name.split(" ");
+  return "Hello Agent ${parts[1][0]}.${parts[0]} your id is $id";
+}
+
 void outer(String name, String id) {
-  void inner() {
-    print("hello Agent $name, your id is $id");}
+  print(inner(name, id));
 }
